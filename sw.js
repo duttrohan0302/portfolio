00b7067fc892a784738cf8edcd544c998d443ded
duttrohan0302 <duttrohan0302@gmail.com>
2022-01-24 23:48:10 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-e4ce7301495663b6c564.js"
+    "url": "webpack-runtime-5896735ee145e3f59ba2.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7b122613058c565eb5709b9f3f53147b"
+    "revision": "be8e2f346517eedf770cd2878f977da0"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-50fcc746a3b9bd6c1224.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "6c87568c7c0a472814c07df5355e09c5"
+    "revision": "13438fe25becab873ad02d9a8090d54d"
   },
   {
     "url": "polyfill-e8f24fa8130299bb1852.js"
